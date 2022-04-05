aec60d9c7c3a9d3f86c59926ed9ae7c46318b3fe
gurpreet1961 <gurpreet012guru@gmail.com>
2022-04-05 17:33:36 +0530

    Home Page Front End

---
+++ client/src/Components/Fundraise.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+const Fundraise = () => {
+  return (
+    <>
+      <div className="Navcontainer">
+        <Navbar />
+        <main>
+          <h1>Fundraise</h1>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Fundraise;
